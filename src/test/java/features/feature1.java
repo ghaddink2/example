a3feeb4b35caeb8f5248fbba7aef66f328bfa23d
Geert-Hinke Addink <ghaddink@Geert-Hinkes-MacBook-Pro.local>
2017-05-19 11:40:29 +0200

    introduced first feature that talks to the mock

--- src/test/java/features/feature1.java
+++ src/test/java/features/feature1.java
@@ -2,7 +2,6 @@ package features;
 
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.thucydides.core.annotations.Steps;
-import net.thucydides.core.annotations.WithTag;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import steps.RestSteps;
@@ -14,8 +13,7 @@ public class feature1 {
     RestSteps rest;
 
     @Test
-    @WithTag("BLKNL_DESKTOP")
     public void test1() throws Exception {
-
+		rest.callService();
     }
 }
--- src/test/java/steps/RestSteps.java
+++ src/test/java/steps/RestSteps.java
@@ -1,4 +1,14 @@
 package steps;
 
+import net.thucydides.core.annotations.Step;
+
+import static net.serenitybdd.rest.SerenityRest.when;
+
 public class RestSteps {
+	@Step
+	public void callService() {
+				when().
+				get("http://localhost:9080/aap/123").
+				then().statusCode(200);
+	}
 }
