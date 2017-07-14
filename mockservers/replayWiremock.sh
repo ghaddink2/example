704359671f64067b615a65163bb02a417281f6c6
Geert-Hinke Addink <gaddink2xebia.com>
2017-07-14 11:26:29 +0200

    set wiremock to port 9080

--- mockservers/replayWiremock.sh
+++ mockservers/replayWiremock.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar wiremock-standalone-2.6.0.jar --port 8080 --https-port 8443 --proxy-all="https://api.openaq.org/"
+java -jar wiremock-standalone-2.6.0.jar --port 9080 --https-port 8443 --proxy-all="https://api.openaq.org/"
